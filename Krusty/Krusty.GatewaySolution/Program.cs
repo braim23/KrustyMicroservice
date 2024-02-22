1cdc2e7c019c159b183df5e4bdd8660930aabb47
braim23 <94292623+braim23@users.noreply.github.com>
2024-02-22 04:29:13 +0300

    Adding Ocelot

---
+++ Krusty/Krusty.GatewaySolution/Program.cs
@@ -0,0 +1,17 @@
+using Krusty.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.AddAppAuthentication();
+
+builder.Services.AddOcelot();
+var app = builder.Build();
+
+
+
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+app.Run();
